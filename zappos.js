b8a5b89f195d8c2a0c2cce771422a88e68a00313
Rohan Nethala <afortyinchvertical@gmail.com>
2025-07-02 18:21:01 -0400

    I completed entering the guest email

--- zappos.js
+++ zappos.js
@@ -11,6 +11,12 @@ function waitForSelectorAndClick(page, selector){
     })
 }
 
+function typeStuffIn(page, selector, text){
+    page.waitForSelector(selector).then(() => {
+        page.type(selector, text)
+    })
+}
+
 async function givePage(){
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
@@ -23,15 +29,38 @@ async function addToCart(page){
     await new Promise(resolve => setTimeout(resolve, 2000));
 
     await waitForSelectorAndClick(page, 'label[for="radio-111385-9142933"]')
+    await new Promise(resolve => setTimeout(resolve, 2000));
     await waitForSelectorAndClick(page, 'button[id="add-to-cart-button"]')
     await waitForSelectorAndClick(page, 'button[class="bN-z"]')
-    
+
+
+}
+
+async function enterGuestEmail(page){
+    await page.waitForNavigation()
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    await waitForSelectorAndClick(page, 'input[data-testid="guestEmailInput"]')
+    await typeStuffIn(page, 'input[data-testid="guestEmailInput"]', "rnethbusi@gmail.com")
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    await typeStuffIn(page, 'input[data-testid="guestConfirmEmailInput"]', "rnethbusi@gmail.com")
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    await waitForSelectorAndClick(page, 'button[data-testid="guestSubscriptionCheckbox"]')
+    await waitForSelectorAndClick(page, 'button[data-testid="continueAsGuest"]')
+}
+
+async function checkout(page){
+    await page.waitForNavigation()
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    await waitForSelectorAndClick(page, 'button[data-testid="creditCard"]')
+    await waitForSelectorAndClick(page, 'button[data-testid="continueAsGuest"]')
 }
 
 async function run(){
     let page = await givePage();
     await page.goto(URL)
     await addToCart(page)
+    await enterGuestEmail(page)
 }
 
 run();
