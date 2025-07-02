dc0c74c3423b6ce2e5714a871e9382043279b9db
Rohan Nethala <afortyinchvertical@gmail.com>
2025-07-02 17:53:18 -0400

    Had to switch to zappos

---
+++ zappos.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+
+puppeteer.use(StealthPlugin());
+
+const URL = "https://www.zappos.com/p/mens-nike-sportswear-icon-futura-tee-magic-ember/product/9142933/color/842746"
+
+function waitForSelectorAndClick(page, selector){
+    page.waitForSelector(selector).then(() => {
+        page.click(selector)
+    })
+}
+
+async function givePage(){
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    return page;
+}
+
+
+
+async function addToCart(page){
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    await waitForSelectorAndClick(page, 'label[for="radio-111385-9142933"]')
+    await waitForSelectorAndClick(page, 'button[type="submit"]')
+}
+
+async function run(){
+    let page = await givePage();
+    await page.goto(URL)
+    await addToCart(page)
+}
+
+run();
